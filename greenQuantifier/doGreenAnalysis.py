b636b1ee22ece9ddb4f3b87a592635c0752d66af
zer <zach.earos@gmail.com>
2014-11-03 18:13:14 +0000

    Added functions to take command line arguments

---
+++ greenQuantifier/doGreenAnalysis.py
@@ -0,0 +1,13 @@
+#%%writefile doGreenAnalysis.py
+#!/usr/bin/env python
+from argparse import ArgumentParser
+from greenanalyser import greenAnalyser
+if __name__ == "__main__":
+    parser = ArgumentParser(description = "Generate Green Map and Plot")
+    parser.add_argument('--region1', '-r1')
+    parser.add_argument('--region2','-r2')
+    arguments= parser.parse_args()
+    if arguments.region1 and arguments.region2:
+        greenAnalyser(arguments.region1,arguments.region2)
+    else:
+        print 'You need to provide two locations for us to get along.'
